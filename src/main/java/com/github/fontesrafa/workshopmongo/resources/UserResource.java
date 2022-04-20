ad9266d81e569963c410774ccf33ebb528764eeb
fontesrafa <rafaelfontes777@hotmail.com>
2022-04-20 18:31:19 -0300

    User insertion with POST

--- src/main/java/com/github/fontesrafa/workshopmongo/resources/UserResource.java
+++ src/main/java/com/github/fontesrafa/workshopmongo/resources/UserResource.java
@@ -1,5 +1,6 @@
 package com.github.fontesrafa.workshopmongo.resources;
 
+import java.net.URI;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
@@ -13,8 +14,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 import org.springframework.web.bind.annotation.RequestParam;
 
 
@@ -37,6 +41,14 @@ public class UserResource {
         User user = userService.findById(id);
         UserDTO userDTO = new UserDTO(user);
         return ResponseEntity.ok().body(userDTO);
+    }
+
+    @PostMapping
+    public ResponseEntity<Void> insert(@RequestBody UserDTO userDTO) {
+        User user = userService.fromDTO(userDTO);
+        user = userService.insert(user);
+        URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(user.getId()).toUri();
+        return ResponseEntity.created(uri).build();
     }
     
 }
--- src/main/java/com/github/fontesrafa/workshopmongo/services/UserService.java
+++ src/main/java/com/github/fontesrafa/workshopmongo/services/UserService.java
@@ -3,6 +3,7 @@ package com.github.fontesrafa.workshopmongo.services;
 import java.util.List;
 
 import com.github.fontesrafa.workshopmongo.domain.User;
+import com.github.fontesrafa.workshopmongo.dto.UserDTO;
 import com.github.fontesrafa.workshopmongo.repositories.UserRepository;
 import com.github.fontesrafa.workshopmongo.services.exception.ObjectNotFoundException;
 
@@ -23,4 +24,13 @@ public class UserService {
     public User findById(String id) {
         return userRepository.findById(id).orElseThrow(() -> new ObjectNotFoundException("Object Not Found"));
     }
+
+    public User insert(User obj) {
+        return userRepository.insert(obj);
+    }
+
+    public User fromDTO(UserDTO userDTO) {
+        return new User(userDTO.getId(), userDTO.getName(), userDTO.getEmail());
+    }
+
 }
