0157a3fa7204934e774b49bf84a9f8f1685d4b8d
fontesrafa <rafaelfontes777@hotmail.com>
2022-04-20 17:49:31 -0300

    searching User by ID

--- src/main/java/com/github/fontesrafa/workshopmongo/resources/UserResource.java
+++ src/main/java/com/github/fontesrafa/workshopmongo/resources/UserResource.java
@@ -12,8 +12,11 @@ import com.github.fontesrafa.workshopmongo.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestParam;
+
 
 @RestController
 @RequestMapping(value="/users")
@@ -28,4 +31,12 @@ public class UserResource {
         List<UserDTO> listDTO = list.stream().map(user -> new UserDTO(user)).collect(Collectors.toList());
         return ResponseEntity.ok().body(listDTO);
     }
+
+    @GetMapping(value="/{id}")
+    public ResponseEntity<UserDTO> findById(@PathVariable String id) {
+        User user = userService.findById(id);
+        UserDTO userDTO = new UserDTO(user);
+        return ResponseEntity.ok().body(userDTO);
+    }
+    
 }
--- src/main/java/com/github/fontesrafa/workshopmongo/services/UserService.java
+++ src/main/java/com/github/fontesrafa/workshopmongo/services/UserService.java
@@ -4,7 +4,9 @@ import java.util.List;
 
 import com.github.fontesrafa.workshopmongo.domain.User;
 import com.github.fontesrafa.workshopmongo.repositories.UserRepository;
+import com.github.fontesrafa.workshopmongo.services.exception.ObjectNotFoundException;
 
+import org.apache.logging.log4j.message.ReusableMessage;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -14,7 +16,11 @@ public class UserService {
     @Autowired
     private UserRepository userRepository;
     
-    public List<User> findAll(){
+    public List<User> findAll() {
         return userRepository.findAll();
+    }
+
+    public User findById(String id) {
+        return userRepository.findById(id).orElseThrow(() -> new ObjectNotFoundException("Object Not Found"));
     }
 }
